6c1d08997926d452f93cced4eb75b308a6a6a999
arkanoider <github.913zc@simplelogin.com>
2026-03-19 11:16:02 +0100

    chore: cargo fmt fix

--- src/settings.rs
+++ src/settings.rs
@@ -201,7 +201,6 @@ with your real keys before running Mostrix again.",
     settings.currencies_filter = Vec::new();
     settings.mostro_pubkey =
         "82fa8cb978b43c79b2156585bac2c011176a21d2aead6d9f7c575c005be88390".to_string();
-         
 
     // Serialize to TOML.
     let toml_string = toml::to_string_pretty(&settings)
